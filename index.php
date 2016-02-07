6a2fa3efb027509efd623e43b201115974df9c8f
cuong nguyen <cuongnguyen@cuongs-MacBook-Air.local>
2016-02-07 13:04:36 +0200

    Final commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cuong Nguyen's Portfolio Resume</title>
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <section>
+	    <div class="navbar navbar-default" role="navigation"> 
+	  		<div class="container"> 
+	  			<div class="navbar-header"> 
+	  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	  					<span class="sr-only">Toggle navigation</span>
+	  					<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">CuongNguyen</a>
+				</div>
+				<div class="navbar-collapse collapse"> 
+					<ul class="nav navbar-nav navbar-right"> 
+						<li class="active"><a href="index.html">Home</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="http://cuongnguyen.xyz/wp/">My Wordpress</a></li>
+							<li><a href="cake/index.html">Cake Shop</a></li>
+							<li><a href="blog/index.html">IT Blog</a></li>
+							<li><a href="gallery/index.html">Photo Gallery</a></li>
+							<li><a href="addressbook/index.php">Address Book</a></li>
+							<li><a href="chat-box/index.php">Chat Box</a></li>
+						</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+      <div class="container container-main">
+        <img src="img/person.jpg" class="profile-img">
+        <header>
+          <h1>HELLO, <br> my name is <strong>Cuong Nguyen</strong> and I am a web developer</h1>
+        </header>
+        <img src="img/line.png" class="line">
+        <div class="row">
+          <div class="col-md-7">
+            <h2># About Me</h2>
+            <p>I am able to design and program Internet services and applications for PC, Linux, Mac and mobile environments. I have essential web development skills related to current Internet technologies and protocols, web graphics and multimedia, web authoring and web programming. In addition, I can develop good software and implement security levels. I work well individually as well as a member of a team. I always ensure that I stay up to date with new changes in technology by reading of updates and testing new products. </p>
+          </div>
+          <div class="col-md-5">
+            <h2># Contact Info</h2>
+            <ul class="contact-list">
+              <li><img src="img/phone.png"> 0401812401</li>
+              <li><img src="img/email.png"> iris.aphylla.13@gmail.com</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-7">
+            <h2># Education & Experience</h2>
+
+          <div class="media">
+              <div class="media-left">
+                2012 - now
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Metropolia University of Applied Sciences</h4>
+                <p>Information Technology</p>
+              </div>
+            </div>
+
+			<div class="media">
+              <div class="media-left">
+                2015 - now
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Web development, customer service and logistics</h4>
+                <p>Roidu Ltd.</p>
+              </div>
+            </div>   
+
+            <div class="media">
+              <div class="media-left">
+                2014 - 2015
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Personal assistant</h4>
+                <p>Tuusulan kunta</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-5">
+            <h2># Skills</h2>
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;width: 100%;">
+                HTML5 / CSS3
+              </div>
+            </div>
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 100%;">
+                JavaScript
+              </div>
+            </div>
+
+             <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 90%;">
+                Bootstrap
+              </div>
+            </div>
+
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 90%;">
+                Angular JS
+              </div>
+            </div>
+
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 90%;">
+                Wordpress
+              </div>
+            </div>
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 85%;">
+                PHP & MySQL
+              </div>
+            </div>
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 80%;">
+                Java
+              </div>
+            </div>
+
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 80%;">
+                C/C++
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-7">
+            <h2># Services Offered</h2>
+              <div class="alert alert-success" role="alert"><i class="glyphicon glyphicon-ok"></i> HTML5/CSS3 Website Design</div>
+              <div class="alert alert-info" role="alert"><i class="glyphicon glyphicon-ok"></i> Web Programming (PHP, Rails, Python)</div>
+              <div class="alert alert-success" role="alert"><i class="glyphicon glyphicon-ok"></i> Server & Database Administrator</div>
+              <div class="alert alert-info" role="alert"><i class="glyphicon glyphicon-ok"></i> CMS Web Development (Joomla, Wordpress & Drupal)</div>
+              <div class="alert alert-success" role="alert"><i class="glyphicon glyphicon-ok"></i> PSD to HTML or CMS</div>
+          </div>
+          <div class="col-md-5">
+            <h2># Recent Projects</h2>
+            <ul class="work-list">
+              <li><a href="cake/index.html"><img src="img/work1.jpg"></a></li>
+              <li><a href="blog/index.html"><img src="img/work3.jpg"></a></li>
+              <li><a href="gallery/index.html"><img src="img/work4.jpg"></a></li>
+	          <li><a href="addressbook/index.php"><img src="img/work5.jpg"></a></li>
+              <li><a href="chat-box/index.php"><img src="img/work6.jpg"></a></li>
+              <li><a href="http://cuongnguyen.xyz/wp/"><img src="img/wordpress.jpg"></a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <footer>
+      <div class="container">
+        <p>Copyright &copy; 2015, All Rights Reserved</p>
+        Total visits: <?php
+    			$count_my_page = ("hitcounter.txt");
+    			$hits = file($count_my_page);
+    			$hits[0] ++;
+    			$fp = fopen($count_my_page , "w");
+    			fputs($fp , "$hits[0]");
+    			fclose($fp);
+    			echo $hits[0];
+    		?>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
